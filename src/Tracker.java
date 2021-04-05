d2ee7e944d8d1bf277237e6e237977af11cf67a4
xrisaD <xrdikonimaki@hotmail.com>
2021-04-05 19:07:51 +0300

    protocol defined

--- src/Tracker.java
+++ src/Tracker.java
@@ -4,9 +4,9 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Tracker {
     ConcurrentHashMap<String, String> Registered_peers;
-    ConcurrentHashMap<int, Info> TokenId_toInfo;
-    ConcurrentHashMap<String, ArrayList<int>> Files_toToken;
-    ArrayList<int> All_tokenids;
+    ConcurrentHashMap<Integer, Info> TokenId_toInfo;
+    ConcurrentHashMap<String, ArrayList<Integer>> Files_toToken;
+    ArrayList<Integer> All_tokenids;
     ArrayList<String> All_files;
 
 
